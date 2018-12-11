6f302384fb8029be363d13e4ba9e42332d029429
Yao <boyao@mackenzieinvestments.com>
2018-12-11 15:14:41 -0500

    Add argument to specify output_file_name

--- analysis.py
+++ analysis.py
@@ -3,7 +3,8 @@ personality insights analysis demo.
 """
 from os.path import join, dirname
 import json
-import csv
+import sys
+import argparse
 
 from watson_developer_cloud import PersonalityInsightsV3
 
@@ -31,16 +32,22 @@ def extract(file_name):
         raise NotImplementedError(f'Unsupported format, file name is {file_name}')
 
 
-def driver(file_name):
+def write(data, file_name):
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent=2)
+
+
+def driver(input_file_name, output_file_name):
     service = init_service()
-    input_data = extract(file_name)
+    input_data = extract(input_file_name)
     output_data = service.profile(input_data, content_type='text/plain', raw_scores=True).get_result()
-    json.dumps(output_data, indent=2)
+    write(output_data, output_file_name)
 
 
 def main():
     parser = argparse.ArgumentParser(description="driver")
-    parser.add_argument("--file_name", default="sample.txt")
+    parser.add_argument("--input_file_name", default="sample.txt")
+    parser.add_argument("--output_file_name", default="output.txt")
     parsed_args = vars(parser.parse_args(sys.argv[1:]))
     driver(**parsed_args)
 
